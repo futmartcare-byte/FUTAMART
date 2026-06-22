5be933b2b6251163bb30b6b8474dbe9e59dd7cef
futmartcare <your@email.com>
2026-06-23 00:26:33 +0300

    Fix base path for Cloudflare

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// Fix for __dirname in modern ES module projects
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/FUTAMART/', // Matches your repository name exactly
+  base: '/',
   logLevel: 'error',
   plugins: [
     react(),
