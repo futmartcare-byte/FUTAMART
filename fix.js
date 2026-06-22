a624c8b31b92f9be4eae30c4cae48791b11f6913
futmartcare <your@email.com>
2026-06-23 02:39:41 +0300

    Fix AIChatBot anchor tags via script

---
+++ fix.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+const fixed = fs.readFileSync('src/components/AIChatBot.jsx', 'utf8')
+  .replace(/\n(\s+)(href="mailto:futmartcares@gmail\.com")/g, '\n<a ')
+  .replace(/\n(\s+)(href="mailto:futmartcares@gmail\.com\?subject)/g, '\n<a ')
+  .replace(/<a (<a )/g, '<a ');
+fs.writeFileSync('src/components/AIChatBot.jsx', fixed, 'utf8');
+console.log('done, length:', fixed.length);
