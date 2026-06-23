f244398bdaf3f49716114619b2aa27b5494d2f55
futmartcare <your@email.com>
2026-06-23 03:12:41 +0300

    Launch FUTAMART

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.futamart.app',
+  appName: 'FUTAMART',
+  webDir: 'dist'
+};
+
+export default config;
