cfc87473d32bdf6811a00a13f76cd2345b3f95bf
futmartcare <your@email.com>
2026-06-24 03:30:55 +0300

    Fix App.jsx style backtick syntax error

---
+++ fixapp.cjs
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('src/App.jsx', 'utf8');
+c = c.replace(/\<style\>\{\\n/g, '<style>{\n');
+c = c.replace(/\<\/style\>/g, '</style>');
+fs.writeFileSync('src/App.jsx', c, 'utf8');
+console.log('done, length:', c.length);
