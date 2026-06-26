0e48afbdcf6587ec14407f661d3249327c8f77b4
futmartcare <your@email.com>
2026-06-27 02:23:51 +0300

    Spectate mode colors + Bump SW to v22

--- public/sw.js
+++ public/sw.js
@@ -24,7 +24,7 @@ messaging.onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'futamart-v21';
+const CACHE_NAME = 'futamart-v22';
 const urlsToCache = ['/', '/offline.html'];
 
 self.addEventListener('install', (event) => {
