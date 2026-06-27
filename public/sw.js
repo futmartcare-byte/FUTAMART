1250f2350fc8a7ae58c3343df2b439ff803ce2ad
futmartcare <your@email.com>
2026-06-27 04:21:50 +0300

    Bump SW cache to v11 to show waitlist tab in admin

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-﻿importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
@@ -98,3 +98,4 @@ self.addEventListener('notificationclick', (event) => {
     })
   );
 });
+
